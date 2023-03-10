a45d62bf14de52703615499fea6afc12024c95bf
가동재 <es-kbsys24@hybecorp.com>
2023-03-10 19:44:47 +0900

    learning vue.js 2023.03.10

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <h1 class="title">vue.js</h1>
+
   <!-- npm run serve 스타트 -->
   <!-- 데이터 바인딩 시작 -->
   <!-- 데이터 바인딩을 하는 이유 html에 하드코딩해놓으면 나중에 변경하기 어려움 -->
@@ -19,6 +20,7 @@
     <!-- 데이터 바인딩 방식 {{오브젝트 이름}} -->
     <!-- <button type="button" v-on:click="countUp">허위매물 신고</button> <span>신고수 : {{count}}</span> -->
   </div>
+
   <div class="content">
     <ul class="list">
       <CardItem v-for="(a, i) in oneroom" :key="i" :dataFile="oneroom[i]" />
@@ -26,6 +28,7 @@
   </div>
 
   <ComOne />
+
   <!-- 사용 -->
 
   <Modal :dataFile="oneroom" :click="click" :modal="modal" />
@@ -44,6 +47,7 @@ export default {
   name: "App",
   data() {
     return {
+      people: { name: "kar", age: "29" },
       click: 0,
       oneroom: dummy,
       modal: false,
@@ -77,6 +81,7 @@ export default {
 
 <style>
 /* 임시 타이틀 */
+
 .title {
   font-size: 40px;
   text-align: center;
@@ -85,6 +90,7 @@ export default {
 }
 
 /* 임시 메뉴 */
+
 .menu {
   height: 60px;
   display: flex;
@@ -101,6 +107,7 @@ export default {
 }
 
 /* 임시 콘텐츠 구역 */
+
 .content {
   margin: 20px 0 20px 0;
 }
@@ -133,12 +140,14 @@ export default {
 .list li .box-title:hover {
   text-decoration: underline;
 }
+
 .list li p {
   display: block;
   margin: 5px 0;
 }
 
 /* 임시 모달창 */
+
 .bg {
   width: 100%;
   height: 100%;
@@ -170,61 +179,135 @@ export default {
 }
 </style>
 
-/* 뷰 반복문 */ /* v-for 문법 */ /*
-<li
-  v-for="??(작명) in ??(반복횟수)"
-  :key="작명했던거"
-><button type="button">Home</button></li>
-*/ /* 꼭 키 값을 입력*/ /* 반복횟수에 데이터를 삽입 가능하다 */ /*
+/* 뷰 반복문 */ 
+/* v-for 문법 */ /*
+<li v-for="??(작명) in ??(반복횟수)" :key="작명했던거"><button type="button">Home</button></li>
+*/ 
+
+/* 꼭 키 값을 입력*/ /* 반복횟수에 데이터를 삽입 가능하다 */ /*
 <li
   v-for="(item, i) in menus"
   :key="item"
 ><button type="button">{{item}}</button></li>
-*/ /* 데이터를 삽입하면 데이터 수 만큼 반복 */ /* 그리고 키값을 item에 입력을
-하면 바인딩 가능 */ /* 개념을 다시 정리 반복횟수에 데이터를 삽입하면 작명 했던
-변수는 데이터 안의 자료가 된다. */ /* 키의 용도는 반복으로 생성한 요소들이
-구분하기 위한 속성 */ /* v-for="(item,i) 이런식으로 두개 입력도 가능 여기서 i는
-1씩 증가하는 값*/ /* 이벤트 핸들러 */ /* 클릭 이벤트 */ /*
-<button
-  type="button"
-  v-on:click="자바스크립트 입력하세요"
+*/ 
+
+/* 데이터를 삽입하면 데이터 수 만큼 반복 */ 
+
+/* 그리고 키값을 item에 입력을
+하면 바인딩 가능 */ 
+/* 개념을 다시 정리 반복횟수에 데이터를 삽입하면 작명 했던
+변수는 데이터 안의 자료가 된다. */ 
+
+/* 키의 용도는 반복으로 생성한 요소들이 구분하기 위한 속성 */ 
+
+/* v-for="(item,i) 이런식으로 두개 입력도 가능 여기서 i는 1씩 증가하는 값*/ 
+/* 이벤트 핸들러 */ 
+/* 클릭 이벤트 */ 
+
+/*
+<button type="button" v-on:click="자바스크립트 입력하세요"
 >허위매물 신고</button>
 <span>신고수 : 0</span>
-*/ /*
+*/ 
+
+
+/*
 <button type="button" @:click="">허위매물 신고</button>
 <span>신고수 : 0</span>
-*/ /* 이런식도 가능 */ /*
+*/ 
+
+
+/* 이런식도 가능 */ 
+
+/*
 <button type="button" v-on:click="count++">허위매물 신고</button>
 <span>신고수 : {{count}}</span>
-*/ /* 이벤트명 정리 */ /* v-on:click @:click v-on:mouseover @:mouseover 인풋일
-경우 v-on:input @:input */ /* 자바스크립트가 너무 길 경우 함수로 써라 */ /*
-data() 밑에 methods : 옵션 쓰고 함수명(){자바스크립트 작성} */ /* 함수안에서
-데이터를 쓸 땐 this.데이터명 이런식으로 써라 */ /* 문제풀이 완료 2023 03 08 */
-/* 2023 03 09*/ /* 모달창 만들기 */ /* 개발팁 데이터부터 어떤식으로 만들지 생각
-*/ /* UI는 간단하게 생각해라 그냥 만들고 숨기고 클릭하면 나타난다. */ /* 동적인
-UI만드는 법 */ /* 1. UI의 현재 상태를 데이터로 저장해둠 - 그 UI가 지금
-어떤식으로 보여야함? */ /* 2. 데이터에 따라 UI가 어떻게 보일지 작성 */ /* v-if
-문법(조건식) v-if="1 == 1" 안에 있는 조건이 참일 경우 실행해준다 */ /* import /
-export 문법 다른 파일에서 현재 파일에 있는 변수를 사용하고 싶을 때 사용 보통
-데이터가 길 경우 따로 파일을 생성해준다. var aaa = 10; exprot default aaa aaa란
-변수를 종결해준다. 맨 끝에서 aaa 뱉어진다. 맨 마지막에 작성 dummy.js 마지막에
-export default 변수명; export {변수명1,변수명2}; 아무거나 선택 그리고 다른
-파일에서 import를 script에서 해준다. import 변수명 from './assets/dummy.js';
-import {변수명2}from './assets/dummy.js'; import 해서 객체 안에 삽입이 가능하다.
-*/ /* 모달창 내에 상세페이지 만들기 */ /* 저장소를 만들고 즉 상태를 저장할 곳을
-만들고 리액트로 치면 useState v-on:click="modal = true, click = i" 두개의 이벤트
-삽입가능 클릭을 하면 변하는 값을 할당해준다. */ /* 조건식 추가 */ /* v-else -
-위에게 참이 아니면 v-else를 실행한다. v-else - v-if가 참이 아니면 v-else를
-실행한다. if문을 연달아 사용하고 싶은 경우 v-else-if ex/ v-if="1 == 2" 땡
-v-else-if="3 == 3" 굿 */ /* 컴포넌트 문법 */ /* 긴 html을 한단어로 줄이는 것 */
-/* 왜써? */ /* 코드의 가독성 */ /* 재사용 쉬움 */ /* 초보 특 - 온갖거 다
-컴포넌트로 만들어둠 그러지말고 반복적으로 출현할 부분만 컴포넌트화 해라 */ /*
-한단어 축약한 컴포넌트 쓰는 법 1.vue 파일 import하고 2.등록하고 3.쓰셈 */ /* 1-
-스크립트에 import 2- components 오브젝트에 등록 3- 써라 */ /* props 문법 */ /*
-데이터는 한곳에 밀어넣고 가져다 쓰는 형식 */ /* ex app.vue 부모 컴포넌트
-modal.vue 자식 컴포넌트 자식컴포넌트가 부모가 가지고 있는 데이터를 쓰려면
-props로 전송 1. 데이터를 전송 v-vind or : */ /* : 용도 props 및 html 태그 속성
-바인딩 용 */ /* 2. 등록 3. 사용
-<Modal :title="oneroom[click].title" />
-*/ /* props는 재할당 불가 */ /* 학습완료 2023 03 09 */ /* list 하위 li들 card
-컴포넌트로 만들것 */
+*/
+ 
+ /* 이벤트명 정리 */ 
+ 
+ /* v-on:click @:click v-on:mouseover @:mouseover 인풋일
+경우 v-on:input @:input */
+ 
+ /* 자바스크립트가 너무 길 경우 함수로 써라 */ 
+ 
+ /*
+data() 밑에 methods : 옵션 쓰고 함수명(){자바스크립트 작성} */ 
+/* 함수안에서데이터를 쓸 땐 this.데이터명 이런식으로 써라 */ 
+
+/* 문제풀이 완료 2023 03 08 */
+
+
+
+/* 2023 03 09 */ 
+
+/* 모달창 만들기 */ 
+/* 개발팁 데이터부터 어떤식으로 만들지 생각*/
+/* UI는 간단하게 생각해라 그냥 만들고 숨기고 클릭하면 나타난다. */ 
+
+/* 동적인 UI만드는 법 */ 
+/* 1. UI의 현재 상태를 데이터로 저장해둠 - 그 UI가 지금 어떤식으로 보여야함? */ 
+
+/* 2. 데이터에 따라 UI가 어떻게 보일지 작성 */ 
+
+/* v-if 문법(조건식) v-if="1 == 1" 안에 있는 조건이 참일 경우 실행해준다 */ 
+
+/* import /export 문법 다른 파일에서 현재 파일에 있는 변수를 사용하고 싶을 때 사용 보통 데이터가 길 경우 따로 파일을 생성해준다. 
+var aaa = 10; exprot default aaa aaa란변수를 종결해준다. 
+맨 끝에서 aaa 뱉어진다. 맨 마지막에 작성 dummy.js 마지막에
+
+export default 변수명; 
+export {변수명1,변수명2}; 
+아무거나 선택 그리고 다른 파일에서 import를 script에서 해준다. 
+import 변수명 from './assets/dummy.js';
+import {변수명2}from './assets/dummy.js'; 
+import 해서 객체 안에 삽입이 가능하다.
+*/ 
+
+/* 모달창 내에 상세페이지 만들기 */ 
+/* 저장소를 만들고 즉 상태를 저장할 곳을
+만들고 리액트로 치면 useState
+ v-on:click="modal = true, click = i" 두개의 이벤트
+삽입가능 클릭을 하면 변하는 값을 할당해준다. */ 
+/* 조건식 추가 */ 
+/* v-else - 위에게 참이 아니면 v-else를 실행한다. v-else - v-if가 참이 아니면 v-else를
+실행한다. if문을 연달아 사용하고 싶은 경우 v-else-if 
+ex/ v-if="1 == 2" 땡
+v-else-if="3 == 3" 굿 */
+
+ /* 컴포넌트 문법 */ /* 긴 html을 한단어로 줄이는 것 */
+/* 왜써? */ 
+/* 코드의 가독성 */ 
+/* 재사용 쉬움 */ 
+/* 초보 특 - 온갖거 다 컴포넌트로 만들어둠 그러지말고 반복적으로 출현할 부분만 컴포넌트화 해라 */ 
+/*한단어 축약한 컴포넌트 쓰는 법 1.vue 파일 import하고 2.등록하고 3.쓰셈 */ 
+/* 1- 스크립트에 import 
+   2- components 오브젝트에 등록 
+   3- 써라 */ 
+   
+   
+/* props 문법 */ /*
+데이터는 한곳에 밀어넣고 가져다 쓰는 형식 */ 
+
+/* ex app.vue 부모 컴포넌트 modal.vue 자식 컴포넌트 자식컴포넌트가 부모가 가지고 있는 데이터를 쓰려면 props로 전송 
+1. 데이터를 전송 v-bind or : */ 
+/* : 용도 props 및 html 태그 속성 바인딩 용 */ 
+
+/* 
+2. 등록 
+3. 사용 <Modal :title="oneroom[click].title" />
+*/
+
+ /* props는 재할당 불가 */ /
+ * 학습완료 2023 03 09 */ 
+ /* list 하위 li들 card 컴포넌트로 만들것 */
+
+/* props 보낼 때 다양한 자료형 입력가능 배열, 객체 등등
+그냥 문자 같은거 보낼땐
+v-bind & : 없이 보낼수있다.
+숫자형도 이런식으로 보내면 문자 취급 
+
+오브젝트 안에 내용을 보내려면
+ex : people: { name: "kar", age: "29" },
+<com :이름="people.name" :나이="people.age" /> 로 써야하지만
+<com v-bind="people" />로 한번에 보낼 수 있다. 그다지 비추 */
