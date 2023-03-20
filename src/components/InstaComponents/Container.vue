9a2a4799dee51492b1c2f995735641dd616e79e9
가동재 <es-kbsys24@hybecorp.com>
2023-03-20 18:48:25 +0900

    ajax, axios 서버통신, tab, 삼항연산자 학습완료 2023.03.20

---
+++ src/components/InstaComponents/Container.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <div class="tab-content" v-if="step === 0">
+      <Post v-for="(a, i) in instaData" :key="i" :instaData="instaData[i]" />
+    </div>
+    <!--     vue3부터 v-if와 v-for를 한번에 못씀 -->
+    <div class="tab-content" v-if="step === 1">
+      <div class="upload-image"></div>
+      <div class="filters">
+        <div class="filter-1"></div>
+        <div class="filter-1"></div>
+        <div class="filter-1"></div>
+        <div class="filter-1"></div>
+        <div class="filter-1"></div>
+      </div>
+    </div>
+    <div class="tab-content" v-if="step === 2">
+      <div class="upload-image"></div>
+      <div class="write">
+        <textarea class="write-box">write!</textarea>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Post from "./Post";
+export default {
+  name: "ContainerCom",
+  components: {
+    Post,
+  },
+  props: {
+    instaData: Object,
+    step: Number,
+  },
+};
+</script>
+
+<style>
+.tab-content {
+  width: 500px;
+  margin: 10px auto;
+}
+
+.upload-image {
+  width: 100%;
+  height: 450px;
+  background: cornflowerblue;
+  background-size: cover;
+}
+.filters {
+  overflow-x: scroll;
+  white-space: nowrap;
+}
+.filter-1 {
+  width: 100px;
+  height: 100px;
+  background-color: cornflowerblue;
+  margin: 10px 10px 10px auto;
+  padding: 8px;
+  display: inline-block;
+  color: white;
+  background-size: cover;
+}
+.filters::-webkit-scrollbar {
+  height: 5px;
+}
+.filters::-webkit-scrollbar-track {
+  background: #f1f1f1;
+}
+.filters::-webkit-scrollbar-thumb {
+  background: #888;
+  border-radius: 5px;
+}
+.filters::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
+.write-box {
+  border: none;
+  width: 90%;
+  height: 100px;
+  padding: 15px;
+  margin: auto;
+  display: block;
+  outline: none;
+}
+</style>
---
+++ src/page/PageTab.vue
@@ -0,0 +1,71 @@
+<template>
+  <!-- 탭 연습 2023.03.20 -->
+  <div class="tab">
+    <ul class="tab-list">
+      <li
+        v-for="(a, i) in 3"
+        :key="i"
+        :class="count === i ? `tab-item on` : `tab-item`"
+      >
+        <button type="button" @click="countList(i)" class="tab-btn">
+          탭버튼{{ i }}
+        </button>
+      </li>
+    </ul>
+    <div class="tab-content">
+      <div class="tab-conten-list">내용 바꿔치기{{ count }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PageTab",
+  data() {
+    return { count: 0 };
+  },
+  methods: {
+    countList(i) {
+      this.count = i;
+      console.log(this.count);
+    },
+  },
+};
+</script>
+
+<style>
+.tab {
+  width: 500px;
+  margin: 200px auto;
+}
+
+.tab .tab-list {
+  display: flex;
+  height: 30px;
+}
+
+.tab .tab-list .tab-item {
+  width: 33.3333%;
+  background-color: aquamarine;
+}
+
+.tab .tab-list .tab-item.on {
+  background-color: burlywood;
+}
+
+.tab-list .tab-item button {
+  width: 100%;
+  height: 100%;
+  text-align: center;
+}
+
+.tab .tab-content {
+  width: 100%;
+  height: 400px;
+}
+.tab .tab-content .tab-conten-list {
+  width: 100%;
+  height: 100%;
+  background-color: darkcyan;
+}
+</style>
